d10ed718e3aa047a4df7b0136bc59ab30db498f4
Shults Pavel <bmstustudent@yandex.ru>
2021-01-30 21:58:47 +0300

    fix: check likes, login

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -112,7 +112,7 @@ const updateAvatarUser = async (req, res, next) => {
       err.statusCode = ERROR_CODE;
     }
     next(err);
-    console.log.trace(err);
+    console.log(err.stack);
   }
 };
 
