485198b77ec7a747daccc05db389b364f1239940
Shults Pavel <bmstustudent@yandex.ru>
2021-01-31 23:24:30 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { celebrate, Joi, errors } = require('celebrate');
-const usersRoutes = require('./routes/users.js');
-const cardsRoutes = require('./routes/cards.js');
-const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
+const { errors } = require('celebrate');
+// const usersRoutes = require('./routes/users.js');
+// const cardsRoutes = require('./routes/cards.js');
+// const { login, createUser } = require('./controllers/users');
+// const auth = require('./middlewares/auth');
+// eslint-disable-next-line import/no-unresolved
+const cookieParser = require('cookie-parser');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-require('dotenv').config();
+const NotFoundError = require('./errors/not-found-err');
+const routes = require('./routes');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -30,30 +34,36 @@ mongoose.connect(mongoDbUrl, mongooseConnectOptions);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.use(requestLogger); // подключаем логгер запросов
 
 // за ним идут все обработчики роутов
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/^(http|https):\/\/[^ "]+$/),
-  }),
-}), createUser);
+// app.post('/signin', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required().min(8),
+//   }),
+// }), login);
+// app.post('/signup', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required().min(8),
+//     name: Joi.string().min(2).max(30),
+//     about: Joi.string().min(2).max(30),
+//     avatar: Joi.string().pattern(/^(http|https):\/\/[^ "]+$/),
+//   }),
+// }), createUser);
 
-app.use(auth);
+// app.use(auth);
 
-app.use('/users', auth, usersRoutes);
-app.use('/cards', auth, cardsRoutes);
+// app.use('/users', auth, usersRoutes);
+// app.use('/cards', auth, cardsRoutes);
+
+app.use(routes);
+
+app.all('/*', () => {
+  throw new NotFoundError('Not found');
+});
 
 app.use(errorLogger); // подключаем логгер ошибок
 
