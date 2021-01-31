148ff3d07cb6b11725c78f1abd5a03112e2508bd
Shults Pavel <bmstustudent@yandex.ru>
2021-01-31 15:34:31 +0300

    fix: app.js backend

--- backend/app.js
+++ backend/app.js
@@ -11,27 +11,41 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 require('dotenv').config();
 
 const app = express();
-const { PORT = 3000 } = process.env;
-
-app.use('*', cors({
-  origin: 'https://mestobm.students.nomoreparties.xyz',
-  credentials: true,
-}));
-
-const mongoDbUrl = 'mongodb://127.0.0.1:27017';
-const mongooseConnectOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-};
-
-mongoose.connect(mongoDbUrl, mongooseConnectOptions);
+const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const allowedOrigins = [
+  'https://mestobm.students.nomoreparties.xyz',
+  'https://www.mestobm.students.nomoreparties.xyz',
+];
+app.use(cors());
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
+
+app.options('*', cors());
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -52,19 +66,15 @@ app.use('/users', auth, usersRoutes);
 app.use('/cards', auth, cardsRoutes);
 
 app.use(errorLogger);
+
 app.use(errors());
 
 // eslint-disable-next-line no-unused-vars
-app.use((err, _req, res, _next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500 ? 'Внутренняя ошибка сервера' : message,
-    });
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({ message: err.message || 'Внутренняя ошибка сервера>' });
 });
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-unused-vars
-  console.log(`Сервер запущен на порту: ${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
 });
