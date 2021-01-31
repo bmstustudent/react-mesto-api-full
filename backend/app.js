0b86423793b21dde31699898ffc5933e1112c5d1
Shults Pavel <bmstustudent@yandex.ru>
2021-01-31 23:31:03 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,22 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
-// const usersRoutes = require('./routes/users.js');
-// const cardsRoutes = require('./routes/cards.js');
-// const { login, createUser } = require('./controllers/users');
-// const auth = require('./middlewares/auth');
-// eslint-disable-next-line import/no-unresolved
-const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
+const usersRoutes = require('./routes/users.js');
+const cardsRoutes = require('./routes/cards.js');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-err');
-const routes = require('./routes');
+require('dotenv').config();
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const PORT = 3000;
 
-app.use('*', cors({
-  origin: 'https://mestobm.students.nomoreparties.xyz',
-  credentials: true,
-}));
+app.use(cors());
 
-const mongoDbUrl = 'mongodb://127.0.0.1:27017';
+const mongoDbUrl = 'mongodb://127.0.0.1:27017/mestodb';
 const mongooseConnectOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -34,58 +28,52 @@ mongoose.connect(mongoDbUrl, mongooseConnectOptions);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(requestLogger); // подключаем логгер запросов
 
-// за ним идут все обработчики роутов
-// app.post('/signin', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required().min(8),
-//   }),
-// }), login);
-// app.post('/signup', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required().min(8),
-//     name: Joi.string().min(2).max(30),
-//     about: Joi.string().min(2).max(30),
-//     avatar: Joi.string().pattern(/^(http|https):\/\/[^ "]+$/),
-//   }),
-// }), createUser);
+app.use(requestLogger);
 
-// app.use(auth);
-
-// app.use('/users', auth, usersRoutes);
-// app.use('/cards', auth, cardsRoutes);
-
-app.use(routes);
-
-app.all('/*', () => {
-  throw new NotFoundError('Not found');
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
 });
 
-app.use(errorLogger); // подключаем логгер ошибок
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
 
-app.use(errors()); // обработчик ошибок celebrate
+app.use(auth);
 
-// наш централизованный обработчик
-// eslint-disable-next-line no-unused-vars
+app.use('/users', auth, usersRoutes);
+app.use('/cards', auth, cardsRoutes);
+
+app.all('/*', () => {
+  // eslint-disable-next-line no-undef
+  throw new NotFoundError(requestErrors.notFound.URL_MESSAGE);
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+
 app.use((err, req, res, next) => {
-  // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
 
-  res
-    .status(statusCode)
-    .send({
-      // проверяем статус и выставляем сообщение в зависимости от него
-      message: statusCode === 500
-        ? 'ай донт ноу вотс хапенд'
-        : message,
-    });
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+  });
+  next();
 });
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Сервер запущен на порту: ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
