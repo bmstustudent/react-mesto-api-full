21894ae27d8f493850ecebf69997aed662cf7f9c
Shults Pavel <bmstustudent@yandex.ru>
2021-01-31 18:23:27 +0300

    fix: errors code

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const { routerIndex } = require('./routes/index');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 require('dotenv').config();
+const NotFoundError = require('./errors/not-found-err');
 
 const app = express();
 const PORT = 3000;
@@ -30,24 +31,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-app.use('/', routerIndex);
+
 
 app.use(auth);
 
 app.use('/users', auth, usersRoutes);
 app.use('/cards', auth, cardsRoutes);
 
+app.use('/', routerIndex);
 app.use(errorLogger);
 
+// Централизованная обработка ошибок
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { status = 500, message } = err;
+app.use(() => {
+  throw new NotFoundError('The requested resource is not found');
+});
 
-  res.status(status).send({
-    message: status === 500 ? 'На сервере произошёл сбой нахуй,я заебаля уже тебя искать' : message,
-  });
-  next();
+// здесь обрабатываем все ошибки
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({ message: err.message || 'ошибка сервера' });
 });
 
 app.listen(PORT, () => {
