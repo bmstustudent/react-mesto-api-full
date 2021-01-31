7a9452b926f57abbee8a3c2f09f7695d0afa6e56
Shults Pavel <bmstustudent@yandex.ru>
2021-01-31 23:34:54 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -68,7 +68,7 @@ app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+    message: statusCode === 500 ? 'ай донт ноу ватс хаппенд' : message,
   });
   next();
 });
