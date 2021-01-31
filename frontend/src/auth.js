dc4052908b953b6a39e236912a816dc639796357
Shults Pavel <bmstustudent@yandex.ru>
2021-01-31 14:14:04 +0300

    fix: front singn-in

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -20,29 +20,53 @@ export const register = (email, password) => {
     })
 };
 
-export const authorize = (email, password) => {
-  return fetch(`${BASE_URL}/signin`, {
+// export const authorize = (email, password) => {
+//   return fetch(`${BASE_URL}/signin`, {
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ email, password })
+//   })
+//     .then((res => {
+//       let data = res.json();
+//       if (!res.ok) {
+//         return Promise.reject(res.status);
+//       }
+//       return data;
+//     }))
+//     .then((data) => {
+//       if (data.token) {
+//         setToken(data.token);
+//         return data;
+//       }
+//     })
+// };
+
+const getResponseData = (res) => {
+  return res.json().then((json) => {
+    return res.ok ? json : Promise.reject(json)
+  })
+}
+
+
+
+export const authorize = (email, password) =>
+  fetch(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ email, password })
-  })
-    .then((res => {
-      let data = res.json();
-      if (!res.ok) {
-        return Promise.reject(res.status);
-      }
-      return data;
-    }))
-    .then((data) => {
-      if (data.token) {
-        setToken(data.token);
-        return data;
-      }
-    })
-};
+    body: JSON.stringify({ email, password }),
+  }).then((res) => getResponseData(res))
+
+
+
+
+
+
 
 export const getContent = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
