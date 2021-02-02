757dcd78bd437c905fae0fa9d3549ebba31fbcce
Shults Pavel <bmstustudent@yandex.ru>
2021-02-02 14:38:08 +0300

    del

--- frontend/src/components/Login.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AuthForm from './AuthForm';
-
-function Login(props) {
-  return <AuthForm type="Вход" buttonName="Войти" {...props} />;
-}
-
-export default Login;
